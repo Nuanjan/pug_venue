602bd2b877bfcabc8dbd4840741037494db4267a
Nuanjan <nuanjan.schluntz@gmail.com>
2022-07-10 21:21:22 -0400

    update css file by adding media query to make the app responsive

--- src/components/featured/Carrousel.js
+++ src/components/featured/Carrousel.js
@@ -1,72 +1,75 @@
-import React from 'react';
+import React from "react";
 import Slider from "react-slick";
-import Img1 from '../../resources/images/pp1.jpg';
-import Img2 from '../../resources/images/pp2.jpg';
-import Img3 from '../../resources/images/pp3.jpg';
-import Img4 from '../../resources/images/pp4.jpg';
-import Img5 from '../../resources/images/pp5.jpg';
+import Img1 from "../../resources/images/pixie-petey1.jpeg";
+import Img2 from "../../resources/images/pixie-petey2.jpeg";
+import Img3 from "../../resources/images/pixie-petey3.jpeg";
+import Img4 from "../../resources/images/pixie-petey4.jpeg";
+import Img5 from "../../resources/images/pixie-petey5.jpeg";
 
 const Carrousel = () => {
-    const settings = {
-        dots: false,
-        infinite: true,
-        autoplay: true,
-        speed: 300
-    }
-    return (
-        <div
-        className="carrousel_wrapper"
-        style={{
-            height: `${window.innerHeight}px`,
-            overflow: 'hidden'
-        }}
-        >
-            <Slider {...settings}>
-
-                <div>
-                    <div className="carrousel_image"
-                    style={{
-                        background: `url(${Img1})`,
-                        height: `${window.innerHeight}px`
-                    }}
-                    ></div>
-                </div>
-                <div>
-                <div className="carrousel_image"
-                    style={{
-                        background: `url(${Img2})`,
-                        height: `${window.innerHeight}px`
-                    }}
-                    ></div>
-                </div>
-                <div>
-                <div className="carrousel_image"
-                    style={{
-                        background: `url(${Img3})`,
-                        height: `${window.innerHeight}px`
-                    }}
-                    ></div>
-                </div>
-                <div>
-                <div className="carrousel_image"
-                    style={{
-                        background: `url(${Img4})`,
-                        height: `${window.innerHeight}px`
-                    }}
-                    ></div>
-                </div>
-                <div>
-                <div className="carrousel_image"
-                    style={{
-                        background: `url(${Img5})`,
-                        height: `${window.innerHeight}px`
-                    }}
-                    ></div>
-                </div>
-            </Slider>
-            
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+  };
+  return (
+    <div
+      className="carrousel_wrapper"
+      style={{
+        height: `${window.innerHeight}px`,
+        overflow: "hidden",
+      }}
+    >
+      <Slider {...settings}>
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${Img1})`,
+              height: `${window.innerHeight}px`,
+            }}
+          ></div>
         </div>
-    );
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${Img2})`,
+              height: `${window.innerHeight}px`,
+            }}
+          ></div>
+        </div>
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${Img3})`,
+              height: `${window.innerHeight}px`,
+            }}
+          ></div>
+        </div>
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${Img4})`,
+              height: `${window.innerHeight}px`,
+            }}
+          ></div>
+        </div>
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${Img5})`,
+              height: `${window.innerHeight}px`,
+            }}
+          ></div>
+        </div>
+      </Slider>
+    </div>
+  );
 };
 
 export default Carrousel;
--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import Carrousel from './Carrousel';
-import Countdown from './Countdown'
+import React from "react";
+import Carrousel from "./Carrousel";
+import Countdown from "./Countdown";
 const Featured = () => {
-    return (
-        <div>
-
-            <Carrousel/>
-
-           <div className="artist_name">
-               <div className="wrapper">
-                    Pixie & Petey
-               </div>
-            </div> 
-            <Countdown/>
-        </div>
-    );
+  return (
+    <div>
+      <Carrousel />
+      <div className="pug_name">
+        <div className="wrapper">Pixie & Petey</div>
+      </div>
+      <Countdown />
+    </div>
+  );
 };
 
 export default Featured;
