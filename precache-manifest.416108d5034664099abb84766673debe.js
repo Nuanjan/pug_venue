8fd78fb13aeb4268201f94cab75aaa830599aa70
Nuanjan <nuanjan.schluntz@gmail.com>
2022-07-10 21:25:50 -0400

    Updates

---
+++ precache-manifest.416108d5034664099abb84766673debe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43454cf1dbb079ead44bc93c9ed0a831",
+    "url": "/pug_venue/index.html"
+  },
+  {
+    "revision": "dd0626aa1387f711c1c1",
+    "url": "/pug_venue/static/css/main.b40791e7.chunk.css"
+  },
+  {
+    "revision": "a5c553fbb67267c380c6",
+    "url": "/pug_venue/static/js/2.e9136164.chunk.js"
+  },
+  {
+    "revision": "01db29174a5d10e665a9e62af351bae5",
+    "url": "/pug_venue/static/js/2.e9136164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0626aa1387f711c1c1",
+    "url": "/pug_venue/static/js/main.8b4239b7.chunk.js"
+  },
+  {
+    "revision": "15b3875a191f4f4cbd78",
+    "url": "/pug_venue/static/js/runtime-main.c6793a05.js"
+  },
+  {
+    "revision": "1e8058e84e574fc721698d8f81d66998",
+    "url": "/pug_venue/static/media/pixie-petey1.1e8058e8.jpeg"
+  },
+  {
+    "revision": "fda29d5171b44ebeb32468a18bf44f1e",
+    "url": "/pug_venue/static/media/pixie-petey2.fda29d51.jpeg"
+  },
+  {
+    "revision": "0ddddd1652ec5ef44754ddbcb9fea32f",
+    "url": "/pug_venue/static/media/pixie-petey3.0ddddd16.jpeg"
+  },
+  {
+    "revision": "e80262d4fd0d4fcf5460748d8c797df3",
+    "url": "/pug_venue/static/media/pixie-petey4.e80262d4.jpeg"
+  },
+  {
+    "revision": "37f20a0e5fd41e848606fb095add027e",
+    "url": "/pug_venue/static/media/pixie-petey5.37f20a0e.jpeg"
+  }
+]);
